f4103c96ae3ab8f469fcce72b0c180cd8ed1d8f1
Jieun Kim <jieun.k@hotmail.com>
2022-08-16 03:37:29 -0700

    Edit: schema data for credit table

--- database/schema/02.create_credits.sql
+++ database/schema/02.create_credits.sql
@@ -2,6 +2,7 @@ CREATE TABLE credits (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     value INT,
+    used boolean DEFAULT FALSE,
     created_at DATE DEFAULT (DATE(CURRENT_TIMESTAMP)),
     PRIMARY KEY(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
